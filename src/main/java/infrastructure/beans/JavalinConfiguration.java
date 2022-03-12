380b8398777ab5519a3c436b67ca3c64195908cd
Mahdi Bohloul <mehdi.bohlol@tapsi.net>
2022-03-12 20:43:49 +0330

    Comment javalin files to fix tomcat error

--- src/main/java/infrastructure/beans/JavalinConfiguration.java
+++ src/main/java/infrastructure/beans/JavalinConfiguration.java
@@ -1,42 +1,43 @@
-package infrastructure.beans;
-
-import javax.annotation.PreDestroy;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import io.javalin.Javalin;
-import io.javalin.plugin.openapi.OpenApiOptions;
-import io.javalin.plugin.openapi.OpenApiPlugin;
-import io.javalin.plugin.openapi.ui.SwaggerOptions;
-import io.swagger.v3.oas.models.info.Info;
-
-@Configuration
-public class JavalinConfiguration {
-    private Javalin app;
-
-    @Bean
-    public Javalin javalin() {
-        if (app == null) {
-            app = Javalin.create(config -> {
-                config.registerPlugin(new OpenApiPlugin(getOpenApiOptions()));
-            }).start(8000);
-        }
-        return app;
-    }
-
-    private OpenApiOptions getOpenApiOptions() {
-        Info info = new Info()
-                .version("1.0.0")
-                .description("IEMDB API");
-        return new OpenApiOptions(info).path("/swagger-docs")
-                .swagger(new SwaggerOptions("/swagger").title("IEMDB API"));
-    }
-
-    @PreDestroy
-    private void destroy() {
-        if (app != null) {
-            app.stop();
-        }
-    }
-}
+//package infrastructure.beans;
+//
+//import io.javalin.Javalin;
+//import io.javalin.plugin.openapi.OpenApiOptions;
+//import io.javalin.plugin.openapi.OpenApiPlugin;
+//import io.javalin.plugin.openapi.ui.SwaggerOptions;
+//import io.swagger.v3.oas.models.info.Info;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//import javax.annotation.PreDestroy;
+//
+//@Configuration
+//public class JavalinConfiguration {
+//    private Javalin app;
+//
+//    @Bean
+//    @Profile("javalin")
+//    public Javalin javalin() {
+//        if (app == null) {
+//            app = Javalin.create(config -> {
+//                config.registerPlugin(new OpenApiPlugin(getOpenApiOptions()));
+//            }).start(8000);
+//        }
+//        return app;
+//    }
+//
+//    private OpenApiOptions getOpenApiOptions() {
+//        Info info = new Info()
+//                .version("1.0.0")
+//                .description("IEMDB API");
+//        return new OpenApiOptions(info).path("/swagger-docs")
+//                .swagger(new SwaggerOptions("/swagger").title("IEMDB API"));
+//    }
+//
+//    @PreDestroy
+//    private void destroy() {
+//        if (app != null) {
+//            app.stop();
+//        }
+//    }
+//}
--- src/main/java/infrastructure/runner/ApplicationJavalinRunner.java
+++ src/main/java/infrastructure/runner/ApplicationJavalinRunner.java
@@ -1,50 +1,52 @@
-package infrastructure.runner;
-
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Service;
-
-import framework.router.javalin.JavalinRouter;
-import infrastructure.startup.ApplicationStartup;
-import io.javalin.Javalin;
-import io.javalin.http.HttpCode;
-
-@Service(value = "ApplicationJavalinRunner")
-@Lazy
-public class ApplicationJavalinRunner implements ApplicationRunner {
-
-    private final Javalin javalinApp;
-    private final JavalinRouter javalinRouter;
-
-    public ApplicationJavalinRunner(Javalin javalinApp, JavalinRouter javalinRouter) {
-        this.javalinApp = javalinApp;
-        this.javalinRouter = javalinRouter;
-    }
-
-    @Override
-    public void run() {
-        javalinApp.get("/movies", javalinRouter::route)
-                .error(HttpCode.NOT_FOUND.getStatus(), javalinRouter::indexNotFound)
-                .error(HttpCode.FORBIDDEN.getStatus(), javalinRouter::indexForbidden)
-                .post("/voteComment/{user_id}/{comment_id}/{vote}", javalinRouter::route)
-                .get("/voteComment/{user_id}/{comment_id}/{vote}", javalinRouter::route)
-                .post("/watchList/{user_id}/{movie_id}", javalinRouter::route)
-                .get("/watchList/{user_id}/{movie_id}", javalinRouter::route)
-                .post("/rateMovie/{user_id}/{movie_id}/{rate}", javalinRouter::route)
-                .get("/rateMovie/{user_id}/{movie_id}/{rate}", javalinRouter::route)
-                .post("/index/success", javalinRouter::route)
-                .get("/", javalinRouter::route)
-                .get("/actors/{actor_id}", javalinRouter::route)
-                .get("/movies/{movie_id}", javalinRouter::route)
-                .get("/movies/search/{start_year}/{end_year}", javalinRouter::route)
-                .get("/movies/search/{genre}", javalinRouter::route)
-                .get("/watchList/{user_id}", javalinRouter::route)
-                .get("/index/success", javalinRouter::route)
-                .get("/watchList/{user_id}/{movie_id}/remove", javalinRouter::route);
-    }
-
-    @Override
-    public void stop() {
-        Runtime.getRuntime().addShutdownHook(new Thread(ApplicationStartup::stop));
-    }
-
-}
+//package infrastructure.runner;
+//
+//import org.springframework.context.annotation.Lazy;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Service;
+//
+//import framework.router.javalin.JavalinRouter;
+//import infrastructure.startup.ApplicationStartup;
+//import io.javalin.Javalin;
+//import io.javalin.http.HttpCode;
+//
+//@Service(value = "ApplicationJavalinRunner")
+//@Lazy
+//@Profile("javalin")
+//public class ApplicationJavalinRunner implements ApplicationRunner {
+//
+//    private final Javalin javalinApp;
+//    private final JavalinRouter javalinRouter;
+//
+//    public ApplicationJavalinRunner(Javalin javalinApp, JavalinRouter javalinRouter) {
+//        this.javalinApp = javalinApp;
+//        this.javalinRouter = javalinRouter;
+//    }
+//
+//    @Override
+//    public void run() {
+//        javalinApp.get("/movies", javalinRouter::route)
+//                .error(HttpCode.NOT_FOUND.getStatus(), javalinRouter::indexNotFound)
+//                .error(HttpCode.FORBIDDEN.getStatus(), javalinRouter::indexForbidden)
+//                .post("/voteComment/{user_id}/{comment_id}/{vote}", javalinRouter::route)
+//                .get("/voteComment/{user_id}/{comment_id}/{vote}", javalinRouter::route)
+//                .post("/watchList/{user_id}/{movie_id}", javalinRouter::route)
+//                .get("/watchList/{user_id}/{movie_id}", javalinRouter::route)
+//                .post("/rateMovie/{user_id}/{movie_id}/{rate}", javalinRouter::route)
+//                .get("/rateMovie/{user_id}/{movie_id}/{rate}", javalinRouter::route)
+//                .post("/index/success", javalinRouter::route)
+//                .get("/", javalinRouter::route)
+//                .get("/actors/{actor_id}", javalinRouter::route)
+//                .get("/movies/{movie_id}", javalinRouter::route)
+//                .get("/movies/search/{start_year}/{end_year}", javalinRouter::route)
+//                .get("/movies/search/{genre}", javalinRouter::route)
+//                .get("/watchList/{user_id}", javalinRouter::route)
+//                .get("/index/success", javalinRouter::route)
+//                .get("/watchList/{user_id}/{movie_id}/remove", javalinRouter::route);
+//    }
+//
+//    @Override
+//    public void stop() {
+//        Runtime.getRuntime().addShutdownHook(new Thread(ApplicationStartup::stop));
+//    }
+//
+//}
